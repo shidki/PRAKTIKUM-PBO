35a24f6a7a3ffe0b62560b5d9b242ce6f6bd803e
shidki <100418798+shidki@users.noreply.github.com>
2023-05-30 11:07:51 +0700

    Add files via upload

---
+++ praktikum8 dan 9/genericTest/BangunDatar.java
@@ -0,0 +1,9 @@
+/*
+	Nama  File	: BangunDatar.java
+	Pembuat		: Muhamad Ridwan Ash'shidqi
+	NIM			: 24060121130075
+	Deskripsi	: kelas abstract untuk bangun datar
+*/
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+}
