d5e71d82b77f25dd0697a24e0ee71d2efcff8b75
shidki <100418798+shidki@users.noreply.github.com>
2023-06-10 09:07:40 +0700

    Add files via upload

---
+++ pertemuan10/LambdaList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+/*
+	Nama  File	: LambdaList.java
+	Pembuat		: Muhamad Ridwan Ash'shidqi
+	NIM			: 24060121130075
+	Tanggal		: Sabtu, 10 Juni 2023
+	Deskripsi	: Implementasi lambda pada List, digunakan sebagai parameter pada method
+*/
+
+public class LambdaList {
+    public static void main(String[] args){
+        ArrayList<String> mahasiswaList = new ArrayList<>();
+        mahasiswaList.add("Adi");
+        mahasiswaList.add("Bambang");
+        mahasiswaList.add("Cici");
+        mahasiswaList.add("Didi");
+        mahasiswaList.forEach((nama) -> System.out.println(nama));
+    }
+}
